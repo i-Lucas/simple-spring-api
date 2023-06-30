4d4227e029f2c081057aaae6a8779c3ebc34456f
i-Lucas <novo.contato.lucas@gmail.com>
2023-06-30 17:01:28 -0300

    feat: move logic to user service

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,10 +1,8 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,53 +15,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.UserDTO;
-import com.example.demo.handlers.UserNotFoundException;
 import com.example.demo.model.UserModel;
-import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
 
 @RestController
 @RequestMapping("/")
 public class HelloController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping
-    public List<UserModel> hello() {
-        return userRepository.findAll();
+    public List<UserModel> getUsers() {
+        return userService.findAll();
     }
 
     @PostMapping
-    public void createUser(@RequestBody UserDTO request) {
-        System.out.println(request.name());
-        userRepository.save(new UserModel(request));
+    public ResponseEntity<String> createUser(@RequestBody UserDTO request) {
+
+        userService.createUser(request);
+        return ResponseEntity.ok("User created successfully.");
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<String> updateUser(@PathVariable UUID id, @RequestBody UserDTO userDto) {
-        Optional<UserModel> findUserModel = userRepository.findById(id);
+    @PutMapping("/{userId}")
+    public ResponseEntity<String> updateUser(@PathVariable UUID userId, @RequestBody UserDTO userDto) {
 
-        if (!findUserModel.isPresent()) {
-            throw new UserNotFoundException("User not found.");
-        }
-
-        UserModel userModel = findUserModel.get();
-        BeanUtils.copyProperties(userDto, userModel, "id"); // Copia as propriedades, excluindo a propriedade "id"
-
-        userRepository.save(userModel);
+        userService.saveUser(userDto, userId);
         return ResponseEntity.ok("User updated successfully.");
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable UUID id) {
-        Optional<UserModel> findUser = userRepository.findById(id);
 
-        if (!findUser.isPresent()) {
-            throw new UserNotFoundException("User not found.");
-        }
-
-        userRepository.deleteById(id);
-        return ResponseEntity.ok("User deleted successfully."); // 200 OK
+        userService.deleteUserById(id);
+        return ResponseEntity.ok("User deleted successfully.");
     }
 
 }
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,63 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.handlers.UserNotFoundException;
+import com.example.demo.model.UserModel;
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    public UserModel createUser(UserDTO user) {
+        return userRepository.save(new UserModel(user));
+    }
+
+    public UserModel findUserById(UUID id) {
+
+        Optional<UserModel> findUserModel = userRepository.findById(id);
+
+        if (!findUserModel.isPresent())
+            return null;
+
+        return findUserModel.get();
+    }
+
+    public void saveUser(UserDTO userDto, UUID userId) {
+
+        UserModel findUserModel = this.findUserById(userId);
+
+        if (findUserModel == null) {
+            throw new UserNotFoundException("User not found.");
+        }
+
+        UserModel newUserModel = findUserModel;
+        BeanUtils.copyProperties(userDto, newUserModel, "id"); // Copia as propriedades, exceto "id"
+        userRepository.save(newUserModel);
+    }
+
+    public void deleteUserById(UUID id) {
+
+        UserModel findUserModel = this.findUserById(id);
+
+        if (findUserModel == null) {
+            throw new UserNotFoundException("User not found.");
+        }
+
+        userRepository.deleteById(id);
+    }
+}
