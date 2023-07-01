252b76b3998f06e998c667c3e3507def1bd07abf
i-Lucas <novo.contato.lucas@gmail.com>
2023-06-30 23:38:16 -0300

    feat: findUserByEmail method declaration

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.example.demo.repository;
 
 import java.util.UUID;
 import com.example.demo.model.UserModel;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
 
+    UserModel findByEmail(@Param("email") String email);
 }
